83245766b116a988d72291d68c8b239e2827d233
ilker Tekin <ilkertekin_24@hotmail.com>
2021-02-24 20:43:47 +0300

    Add project files.

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+
+        //Read Only'ler Constructorda set edilebilir !
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+           
+        }
+        public Result(bool success) //OverLoading
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}

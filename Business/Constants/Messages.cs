7d8f5807c694604384ba8663862f003627778159
ilker Tekin <ilkertekin_24@hotmail.com>
2021-03-03 12:52:44 +0300

    03.03.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +15,12 @@ namespace Business.Constants
         public  static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists="Aynı isimde ürün var !";
         public static string CheckIfCategoryLimitExceded="Kategori Limiti Aşıldığı için ürün eklenemiyor";
+        public static string AuthorizationDenied="Yetkiniz yoktur!";
+        public static string UserRegistered="Kayıt başarılı";
+        public static string UserNotFound="Kullanıcı bulunamadı";
+        public static string PasswordError="Parola hatası";
+        public static string SuccessfulLogin="Başarılı giriş";
+        public static string UserAlreadyExists="Kullanıcı Mevcut";
+        public static string AccessTokenCreated="Token OLuşturuldu";
     }
 }
